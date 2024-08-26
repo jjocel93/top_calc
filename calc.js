0fac9b98702776a75b9de9a7e76a7f0a977951e5
My Name <jjocel93@gmail.com>
2024-08-26 14:18:27 +0200

    create references in js to be able to make events. Add num-data to html to store data for each button.

---
+++ calc.js
@@ -0,0 +1,7 @@
+// create reference to be able to make events for buttons and display
+const button = document.querySelectorAll('.Btn-number, .Btn-operator');
+
+const display = document.querySelector('.Display');
+
+//create an element that displays the numbers that are clicked
+const buttonSelected = document.createElement('h1');
