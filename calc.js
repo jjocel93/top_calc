e2eed7acf16642e88053477f953eadd48f873c00
My Name <jjocel93@gmail.com>
2024-08-27 01:30:17 +0200

    update js so the calculator can be ran multiple times in a row.

--- calc.js
+++ calc.js
@@ -1,19 +1,19 @@
 //Create functions for operators
 
 function muliplyNumbers(a, b) {
-  return a * b;
+  return parseInt(a) * parseInt(b);
 }
 
 function divideNumbers(a, b) {
-  return a / b;
+  return parseInt(a) / parseInt(b);
 }
 
 function addNumbers(a, b) {
-  return a + b;
+  return parseInt(a) + parseInt(b);
 }
 
 function subtractNumbers(a, b) {
-  return a - b;
+  return parseInt(a) - parseInt(b);
 }
 
 // Create a variable for the first number, the operator, and the second number
@@ -21,12 +21,13 @@ function subtractNumbers(a, b) {
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
+let result = '';
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 function operate(firstNumber, secondNumber, operator) {
   switch (operator) {
     case '+':
-      return addNumbersNumbers(firstNumber, secondNumber);
+      return addNumbers(firstNumber, secondNumber);
     case '-':
       return subtractNumbers(firstNumber, secondNumber);
     case '*':
@@ -43,7 +44,7 @@ const opbuttons = document.querySelectorAll('.Btn-operator');
 
 const display = document.querySelector('.Display');
 
-const equals = document.querySelector('#equals');
+const equals = document.querySelector('.Btn-equals');
 
 const clear = document.querySelector('#clear');
 
@@ -67,8 +68,33 @@ buttons.forEach((button) =>
 // add event listener for operator being selected
 opbuttons.forEach((opbutton) =>
   opbutton.addEventListener('click', () => {
-    const buttonValue = opbutton.getAttribute('data-num');
-    operator += buttonValue;
-    display.textContent = operator;
+    if (result === '') {
+      const buttonValue = opbutton.getAttribute('data-num');
+      operator += buttonValue;
+      display.textContent = operator;
+    } else {
+      const buttonValue = opbutton.getAttribute('data-num');
+      operator = '';
+      operator += buttonValue;
+      display.textContent = operator;
+    }
   })
 );
+
+equals.addEventListener('click', () => {
+  if (result === '') {
+    const equalValue = operate(firstNumber, secondNumber, operator);
+    result += equalValue;
+    display.textContent = result;
+    firstNumber = result;
+    operator = '';
+    secondNumber = '';
+  } else {
+    result = '';
+    const equalValue = operate(firstNumber, secondNumber, operator);
+    result += equalValue;
+    display.textContent = result;
+    firstNumber = result;
+    secondNumber = '';
+  }
+});
