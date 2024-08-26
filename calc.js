3530af75d550a756c07d358dc0b1149575ed445b
My Name <jjocel93@gmail.com>
2024-08-26 23:42:23 +0200

    fix bug for event listeners that store first number, operator, and second number.

--- calc.js
+++ calc.js
@@ -21,7 +21,6 @@ function subtractNumbers(a, b) {
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
-let noOperator = true;
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 function operate(firstNumber, secondNumber, operator) {
@@ -50,25 +49,26 @@ const clear = document.querySelector('#clear');
 
 const decimcal = document.querySelector('#decimal');
 
-//create display variable will take display buttons that are pressed in next function
+// add event listener for button selections depending on if operator is undefined or present
 
-// add event listener for first buttons being selected
+buttons.forEach((button) =>
+  button.addEventListener('click', () => {
+    const buttonValue = button.getAttribute('data-num');
+    if (operator === '') {
+      firstNumber += buttonValue;
+      display.textContent = firstNumber;
+    } else {
+      secondNumber += buttonValue;
+      display.textContent = secondNumber;
+    }
+  })
+);
 
-// buttons.forEach((button) =>
-//   button.addEventListener('click', () => {
-//     const buttonValue = button.getAttribute('data-num');
-//     firstNumber += buttonValue;
-//     display.textContent = firstNumber;
-//   })
-// );
-
-// // add event listener for operator being selected
-// opbuttons.forEach((opbutton) =>
-//   opbutton.addEventListener('click', () => {
-//     const buttonValue = opbutton.getAttribute('data-num');
-//     operator += buttonValue;
-//     display.textContent = operator;
-//   })
-// );
-
-// let storedValue = firstNumber;
+// add event listener for operator being selected
+opbuttons.forEach((opbutton) =>
+  opbutton.addEventListener('click', () => {
+    const buttonValue = opbutton.getAttribute('data-num');
+    operator += buttonValue;
+    display.textContent = operator;
+  })
+);
